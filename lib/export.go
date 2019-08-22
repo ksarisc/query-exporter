2d1abe1035f818af36463e6fa1793ab61335923f
Jacob Smith <JacobSmith.IN@gmail.com>
2019-08-22 17:01:51 -0400

    export to lib for containing all file ops

---
+++ lib/export.go
@@ -0,0 +1,44 @@
+package lib
+
+import (
+	"bufio"
+	"os"
+)
+
+// FileExporter holds buffered file writing capabilities
+type FileExporter struct {
+	Path   string
+	File   *os.File
+	Writer *bufio.Writer
+}
+
+// NewFileExporter validate path and build new FileExporter instance
+func NewFileExporter(path string) (FileExporter, error) {
+	file, err := os.Create(path)
+	if err != nil {
+		return FileExporter{}, err
+	}
+	return FileExporter{
+		Path:   path,
+		File:   file,
+		Writer: bufio.NewWriter(file),
+	}, nil
+} // END NewFileExporter
+
+// WriteString exposes FileExporter as Writer
+func (fe *FileExporter) WriteString(s string) (int, error) {
+	return fe.Writer.WriteString(s)
+} // END WriteString
+
+// Write exposes FileExporter as Writer
+func (fe *FileExporter) Write(p []byte) (int, error) {
+	return fe.Writer.Write(p)
+} // END Write
+
+// Close exposes FileExporter as Writer
+func (fe *FileExporter) Close(p []byte) error {
+	err := fe.Writer.Flush()
+	fe.File.Close()
+	// what about the potential close error?
+	return err
+} // END Close
